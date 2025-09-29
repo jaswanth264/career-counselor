540688f4ba1df427ed1903521cf10f632b5d7cc9
jaswanth264 <jchallag@gitam.in>
2025-09-29 10:50:49 +0530

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+};
